4cc1877d28047707b09fc54825a812bea08e649e
Huwensong <76541850+Huwensong@users.noreply.github.com>
2022-08-01 09:29:41 +0800

    Update
    删除miio_sdk 作为idf 组件编译

--- tools/sdk/include/miio_sdk/esp32/esp32_config.h
+++
@@ -1,8 +0,0 @@
-/* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __ESP32_CONFIG_H__
-#define __ESP32_CONFIG_H__
-
-#define ESP32_GATTS_APP_ID            0
-#define ESP32_GATTC_APP_ID            1
-
-#endif
--- tools/sdk/include/miio_sdk/miio_ble/cmd/mible_rpc_test.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MIBLE_RPC_TEST_H__
-#define __MIBLE_RPC_TEST_H__
-
-int mible_rpc_test(void);
-
-void mible_rpc_add_test(void);
-
-#endif
